c12606ef062a2798712f1747f051f264143621f0
Norman Chan <slowmanchan@gmail.com>
2016-08-18 12:10:28 -0400

    Added graphics changed fonts

--- practice.js
+++ practice.js
@@ -1,69 +1,76 @@
 $(document).ready(function() {
   getNumber();
   getOperator();
-  $(".container").on("click", ".equals", function() {
-    equals(input, operator);
-  })
+  equals();
+  getClear();
 })
 
-var input = [];
+var input = "";
 var operator = "";
 var x1 = 0;
 var x2 = 0;
 
-function equals(input, operator) {
-  getX2(input);
-  switch(operator) {
-    case "+":
-      add(x1, x2);
-      break;
-    case "*":
-      multiply(x1, x2);
-      break;
-    case "-":
-      subtract(x1, x2);
-      break;
-    case "/":
-      divide(x1, x2);
-      break;
-    default:
-      console.log("invalid selection");
-  }
+function equals() {
+  $(".container").on("click", ".equals", function() {
+    x2 = parseInt(input);
+    switch(operator) {
+      case "+":
+        add(x1, x2);
+        break;
+      case "*":
+        multiply(x1, x2);
+        break;
+      case "-":
+        subtract(x1, x2);
+        break;
+      case "/":
+        divide(x1, x2);
+        break;
+      default:
+        console.log("invalid selection");
+    }
+  clear();
+  })
 }
 function getOperator() {
   $(".container").on("click", ".operator", function() {
-    getX1(input);
+    x1 = parseInt(input);
+    window.input = "";
     operator = $(this).text();
-    console.log(operator);
+    display(operator);
+  })
+}
+function getClear() {
+  $(".container").on("click", ".clear", function() {
+    clear();
+    display("0");
+  })
+}
+function getNumber() {
+  $(".container").on("click", ".grid", function() {
+    input += $(this).text();
+    display(input);
   })
 }
 
-function getNumber() {
-  $(".container").on("click", ".grid", function() {
-    input.push($(this).text());
-    console.log();
-  })
+function clear() {
+  input = "";
+  x1 = 0;
+  x2 = 0;
+  operator = "";
 }
-function getX1(input) {
-  x1 = input.join().replace(/,/g, '');
-  x1 = parseInt(x1);
-  console.log(x1);
-  input = [];
-}
-function getX2(input) {
-  x2 = input.join().replace(/,/g, '');
-  x2 = parseInt(x2);
-  console.log(x2);
+function display(input) {
+  $(".answer").html(input);
 }
 function add(x, y) {
-  console.log(x + y)
+  display(x + y);
 }
 function multiply(x, y) {
-  console.log(x * y)
+  display(x * y)
 }
 function divide(x, y) {
-  console.log(x / y)
+  display(x / y)
 }
 function subtract(x, y) {
-  console.log(x - y)
+  display(x - y)
 }
